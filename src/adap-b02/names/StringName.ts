414827df737118fafd456ab0c3be24afd5f9e1ea
karthikraghu <karthiksoraghuram@gmail.com>
2025-10-30 11:53:23 +0100

    b02 - Implement methods in StringArrayName and StringName classes to handle string components and parsing

--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -8,51 +8,127 @@ export class StringName implements Name {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter ?? DEFAULT_DELIMITER;
+        this.name = source;
+        this.noComponents = this.parseComponents().length;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        return components.join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        const escapedComponents = components.map(component => {
+            let escaped = '';
+            for (let char of component) {
+                if (char === DEFAULT_DELIMITER || char === ESCAPE_CHARACTER) {
+                    escaped += ESCAPE_CHARACTER;
+                }
+                escaped += char;
+            }
+            return escaped;
+        });
+        return escapedComponents.join(DEFAULT_DELIMITER);
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents === 0;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        return components[x];
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        components[n] = c;
+        this.name = this.buildString(components);
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        components.splice(n, 0, c);
+        this.name = this.buildString(components);
+        this.noComponents = components.length;
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        components.push(c);
+        this.name = this.buildString(components);
+        this.noComponents = components.length;
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        components.splice(n, 1);
+        this.name = this.buildString(components);
+        this.noComponents = components.length;
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        const components = this.parseComponents();
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            components.push(other.getComponent(i));
+        }
+        this.name = this.buildString(components);
+        this.noComponents = components.length;
+    }
+
+    private parseComponents(): string[] {
+        if (this.name === "") {
+            return [];
+        }
+        
+        const components: string[] = [];
+        let currentComponent = "";
+        let i = 0;
+        
+        while (i < this.name.length) {
+            if (this.name[i] === ESCAPE_CHARACTER && i + 1 < this.name.length) {
+                // Escaped character - add the next character literally
+                currentComponent += this.name[i + 1];
+                i += 2;
+            } else if (this.name[i] === this.delimiter) {
+                // Delimiter - end current component
+                components.push(currentComponent);
+                currentComponent = "";
+                i++;
+            } else {
+                // Regular character
+                currentComponent += this.name[i];
+                i++;
+            }
+        }
+        
+        // Add the last component
+        components.push(currentComponent);
+        return components;
+    }
+
+    private buildString(components: string[]): string {
+        const escapedComponents = components.map(component => {
+            let escaped = '';
+            for (let char of component) {
+                if (char === this.delimiter || char === ESCAPE_CHARACTER) {
+                    escaped += ESCAPE_CHARACTER;
+                }
+                escaped += char;
+            }
+            return escaped;
+        });
+        return escapedComponents.join(this.delimiter);
     }
 
 }
