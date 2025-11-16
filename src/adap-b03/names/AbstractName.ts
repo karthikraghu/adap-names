b8cceb7bc4601fb8dda8079b8fa5be6fc54bc5a0
karthikraghu <karthiksoraghuram@gmail.com>
2025-11-16 16:50:01 +0100

    b03 - Implement methods in AbstractName, StringArrayName, and StringName classes to provide functionality and remove placeholder errors

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -6,7 +6,7 @@ export abstract class AbstractName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter;
     }
 
     public clone(): Name {
@@ -14,7 +14,11 @@ export abstract class AbstractName implements Name {
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        let result: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            result.push(this.getComponent(i));
+        }
+        return result.join(delimiter);
     }
 
     public toString(): string {
@@ -22,23 +26,54 @@ export abstract class AbstractName implements Name {
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        let result: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            let component = this.getComponent(i);
+            let escaped = '';
+            for (let j = 0; j < component.length; j++) {
+                let c = component.charAt(j);
+                if (c === DEFAULT_DELIMITER || c === ESCAPE_CHARACTER) {
+                    escaped += ESCAPE_CHARACTER;
+                }
+                escaped += c;
+            }
+            result.push(escaped);
+        }
+        return result.join(DEFAULT_DELIMITER);
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        if (this.delimiter !== other.getDelimiterCharacter()) {
+            return false;
+        }
+        if (this.getNoComponents() !== other.getNoComponents()) {
+            return false;
+        }
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            if (this.getComponent(i) !== other.getComponent(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        let hashCode: number = 0;
+        const s: string = this.asDataString();
+        for (let i = 0; i < s.length; i++) {
+            let c = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+        return hashCode;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
     abstract getNoComponents(): number;
@@ -51,7 +86,9 @@ export abstract class AbstractName implements Name {
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
 
 }
